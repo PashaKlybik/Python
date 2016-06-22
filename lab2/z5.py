2e06f74056e22b0577501438407ad893fd86d52e
PashaKlybik <pashaklybik@gmail.com>
2016-06-22 19:11:43 +0300

    add z5, z7 and started z9

--- lab2/z5.py
+++ lab2/z5.py
@@ -1,6 +1,43 @@
 __author__ = 'pasha'
-import z4
 
-a= z4.Vector(2)
-print(str(a))
+class Logger(object):
+    log = ""
+    def __getattribute__(self, item):
+        temp = object.__getattribute__(self,item)
 
+        def cmpfunc(*args):
+            result = temp(*args)
+            Logger.log += "Func: '{0}', arg: {1}, result {2}\n".format(item, args, result)
+            return result
+
+        if callable(temp):
+            return cmpfunc
+        return temp
+
+    def __str__(self):
+        #print(Logger.log)
+        return Logger.log
+
+class Test(Logger):
+    def __init__(self, name = "Test object"):
+        self.name = name
+
+    def bar(self):
+        print('bar')
+
+    def mul(self, x, y):
+        return x*y
+
+    def sub(self, x, y):
+        return x-y
+
+def main():
+    a = Test("zxc")
+    a.bar()
+    a.mul(2,4)
+    a.sub(9,6)
+    a.mul(6,4)
+    print(a)
+
+if __name__ == "__main__":
+    main()
--- lab2/z9.py
+++ lab2/z9.py
@@ -1,1 +1,26 @@
 __author__ = 'pasha'
+
+class Xrange():
+    def _check(self):
+        print(self.h,self.start,self.end)
+        if self.h>0:
+            return self.start <= self.end
+        else:
+            return self.end <= self.start
+
+    def __init__(self, end, start = 0, h = 1):
+        self.start = start
+        self.end = end
+        self.h = h
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        while Xrange._check(self):
+            print(self.start)
+            self.start += self.h
+            print
+            yield self.start
+        raise StopIteration
+
